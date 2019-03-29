f84584d3d35e399643444611f3b54f6d7ec65e2d
tirumaladevi1 <45657928+tirumaladevi1@users.noreply.github.com>
2019-03-29 13:14:18 +0530

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get install apache2 -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
-sudo mysql -h127.0.0.1 -P3306 -uroot -e"UPDATE mysql.user SET password = MYSQL_ROOT_PASSWORD="root" WHERE user = 'root'"
-sudo apt-get -y install apache2 mysql-server php5-mysql php5 libapache2-mod-php5 php5-mcrypt
-sudo echo -e "<?php\nphpinfo();\n?>" >> /var/www/html/info.php
 sudo service apache2 restart
